97ac4b8bcb8124b436c323c493fe8bc8686f9656
Saurabh Joshi <64773452+j-saurabh@users.noreply.github.com>
2023-04-06 23:52:29 +0530

    Update and rename kubectl-cmd.txt to kubectl-cmd.sh

--- commands/kubectl-cmd.sh
+++ commands/kubectl-cmd.sh
@@ -10,3 +10,11 @@ kubectl logs <pod-name> -n <namespace>                        //Logs for a parti
 kubectl top nodes                                             //List of nodes and information on CPU and memory consumption in decreasing order
 
 kubectl top pods -n <namespace>                               //List the pods which are consuming high CPU and memory
+
+kubectl describe pod <pod-name> -n <namespace>                //List various details about the pod
+
+kubectl delete pod <pod-name> -n <namespace>                  //Delete a pod that is running
+
+kubectl get svc -n <namespace>                                //List the services in the namespace
+
+kubectl describe svc <service-name> -n <namespace>            //List the details about the service
