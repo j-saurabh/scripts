b4414a0554b28084f832da793c74b942d78e2265
Saurabh Joshi <64773452+j-saurabh@users.noreply.github.com>
2023-04-10 23:29:51 +0530

    Update Kubectl-cmd.sh with more commands

--- commands/kubectl-cmd.sh
+++ commands/kubectl-cmd.sh
@@ -1,20 +1,42 @@
 kubectl commands cheat sheet
 ------------------------------
-kubectl cluster-info                                          //List the cluster details
+kubectl cluster-info                                          # List the cluster details
 
-kubectl get pods -n <namespace>                               //List pods in the particular namespace
-kubectl get pods -o wide -n <namespace>                       //List pods with more information
+kubectl get pods -n <namespace>                               # List pods in the particular namespace
+kubectl get pods -o wide -n <namespace>                       # List pods with more information
+kubectl get pod my-pod -o yaml                                # Get the yaml for the pod
+kubectl get pods --field-selector=status.phase=Running        # Get the running pods in the namespace 
 
-kubectl logs <pod-name> -n <namespace>                        //Logs for a particular pod
+kubectl logs <pod-name> -n <namespace>                        # Logs for a particular pod
 
-kubectl top nodes                                             //List of nodes and information on CPU and memory consumption in decreasing order
+kubectl top nodes                                             # List of nodes and information on CPU and memory consumption in decreasing order
 
-kubectl top pods -n <namespace>                               //List the pods which are consuming high CPU and memory
+kubectl top pods -n <namespace>                               # List the pods which are consuming high CPU and memory
 
-kubectl describe pod <pod-name> -n <namespace>                //List various details about the pod
+kubectl describe pod <pod-name> -n <namespace>                # List various details about the pod
 
-kubectl delete pod <pod-name> -n <namespace>                  //Delete a pod that is running
+kubectl delete pod <pod-name> -n <namespace>                  # Delete a pod that is running
 
-kubectl get svc -n <namespace>                                //List the services in the namespace
+kubectl get svc -n <namespace>                                # List the services in the namespace
 
-kubectl describe svc <service-name> -n <namespace>            //List the details about the service
+kubectl describe svc <service-name> -n <namespace>            # List the details about the service
+
+kubectl apply -f ./my-manifest.yaml                           # Create resources based on the manifest file
+kubectl apply -f ./deploy1.yaml -f ./deploy2.yaml             # Create resources from multiple files
+
+kubectl edit deployment <deployment-name>                     # Edit the deployment that is already running 
+
+# Create multiple YAML objects from stdin
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: Pod
+metadata:
+  name: busybox-sleep
+spec:
+  containers:
+  - name: busybox
+    image: busybox:1.28
+    args:
+    - sleep
+    - "1000000"
+---
